0db4573b70086f5d5b46adbc1ce4f003210e2ef3
GenkiOsawa <taiiku39@gmail.com>
2021-04-23 22:40:38 +0900

    テスト

---
+++ index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const PORT = 8080
+
+app.get('/', (req, res) => {
+    res.send('Cloud Runのテストしてみたよ')
+})
+
+app.listen(PORT, () => {
+    console.log(`🚀Running http://localhost:8080`)
+})
